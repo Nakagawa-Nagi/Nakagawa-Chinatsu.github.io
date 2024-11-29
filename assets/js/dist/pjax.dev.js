be1aff0fe182c4e34d63ed01137ad447ea0eb71b
中川-千夏 <116638031+Nakagawa-Nagi@users.noreply.github.com>
2024-11-29 23:56:23 +0800

    videos surpport

---
+++ assets/js/dist/pjax.dev.js
@@ -0,0 +1,9 @@
+// var pjax = new Pjax({selectors: [".pjaxx","title"]});
+// function pjax_reload() {
+//   //其他需要重新加载的函数也可以添加在这里
+// }
+// // Pjax 完成后，重新加载上面的函数
+// document.addEventListener("pjax:complete", function () {
+//     pjax_reload();
+// });
+"use strict";
--- layout/dist/js.dev.js
+++ layout/dist/js.dev.js
@@ -1,18 +1,18 @@
 "use strict";
 
-document.writeln("  <script src=\'https://cdn.bootcdn.net/ajax/libs/aos/2.3.4/aos.js\'></script>");
-document.writeln("  <script src=\'https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.2.2/js/bootstrap.bundle.min.js\'></script>");
-document.writeln("  <script src=\'https://cdn.bootcdn.net/ajax/libs/glightbox/3.2.0/js/glightbox.min.js\'></script>");
-document.writeln("  <script src=\'https://cdn.bootcdn.net/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js\'></script>");
-document.writeln("  <script src=\'https://cdn.bootcdn.net/ajax/libs/validate-js/2.0.1/validate.js\'></script>");
+document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.min.js\'></script>");
+document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js\'></script>");
+document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/glightbox@3.3.0/dist/js/glightbox.min.js\'></script>");
+document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/isotope-layout@3.0.6/dist/isotope.pkgd.min.js\'></script>");
+document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/validate.js@0.13.1/validate.min.js\'></script>");
 document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/@srexi/purecounterjs@1.5.0/js/purecounter.js\'></script>");
-document.writeln("  <script src=\'https://cdn.bootcdn.net/ajax/libs/Swiper/8.4.4/swiper-bundle.min.js\'></script>"); //document.writeln("  <script src=\'../../../../../../live2d_models/lib/L2Dwidget.min.js?094cbace49a39548bed64abff5988b05\'></script>");
-//document.writeln("  <script>L2Dwidget.init({\'pluginRootPath\':\'../../../../../../live2d_models/\',\'pluginJsPath\':\'../../../../../../live2d_models/lib/\',\'pluginModelPath\':\'../../../../../../assets/\',\'tagMode\':false,\'log\':false,\'model\':{\'jsonPath\':\'../../../../../../live2d_models/assets/assets/shizuku.model.json\'},\'display\':{\'position\':\'left\',\'width\':150,\'height\':300},\'mobile\':{\'show\':true}});");
-//document.writeln("  </script>");
+document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/swiper@11.1.15/swiper-bundle.min.js\'></script>"); // document.writeln("  <script src=\'../../../../../../live2d_models/lib/L2Dwidget.min.js?094cbace49a39548bed64abff5988b05\'></script>");
+// document.writeln("  <script>L2Dwidget.init({\'pluginRootPath\':\'../../../../../../live2d_models/\',\'pluginJsPath\':\'../../../../../../live2d_models/lib/\',\'pluginModelPath\':\'../../../../../../assets/\',\'tagMode\':false,\'log\':false,\'model\':{\'jsonPath\':\'../../../../../../live2d_models/assets/assets/shizuku.model.json\'},\'display\':{\'position\':\'left\',\'width\':150,\'height\':300},\'mobile\':{\'show\':true}});");
+// document.writeln("  </script>");
 
 document.writeln("  <script src=\'../../../../../../assets/js/main.js\'></script> ");
 document.writeln("  <script type=\'text/javascript\' color=\'79,172,254\' opacity=\'0.7\' zIndex=\'-1\' count=\'200\' src=\'//cdn.bootcss.com/canvas-nest.js/1.0.0/canvas-nest.min.js\'></script>");
-document.writeln("<script src=\'https://cdn.bootcdn.net/ajax/libs/pjax/0.2.8/pjax.min.js\'></script>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/pjax@0.2.8/pjax.min.js\'></script>");
 document.writeln("<script src=\'../../../../../../assets/js/pjax.js\'></script>");
-document.writeln("<script src=\'https://cdn.bootcdn.net/ajax/libs/aplayer/1.10.1/APlayer.min.js\'></script>");
-document.writeln("<script src=\'https://cdn.bootcdn.net/ajax/libs/hls.js/1.4.12/hls.js\'></script>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js\'></script>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/hls.js@1.5.17/dist/hls.min.js\'></script>");
